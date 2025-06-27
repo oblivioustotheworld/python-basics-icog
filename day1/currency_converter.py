a52cba11b2524413c5e951474390bdc6af5cb523
oblivioustotheworld <oblivioustotheworld.reemgedion24@gmail.com>
2025-06-27 11:08:07 +0300

    final version

--- day1/currency_converter.py
+++ day1/currency_converter.py
@@ -1,2 +1,20 @@
+def converter(opt,amount):
+    result=0
+    if opt==1:
+        result=amount*0.0072
+        print('$'+str(result))
+    else:
+        result=amount*135.25
+        print(str(result)+'birr')
+    
+
+
+
+
+
+option=int(input("1.etb to usd  \n2.usd to etb  "))
+amount=int(input("Enter amount  "))
+
+converter(option,amount)
 
 
